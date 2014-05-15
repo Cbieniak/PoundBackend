e17559d649dc63af638265b4e1f991ae7d2866ca
Christian Bieniak <cbieniak@gmail.com>
2014-05-16 00:07:37 +1000

    Updates to pet serilizer

--- PoundBackend/app/models/pet.rb
+++ PoundBackend/app/models/pet.rb
@@ -1,7 +1,7 @@
 class Pet < ActiveRecord::Base
 	mount_uploader :image, PetImageUploader
 	has_and_belongs_to_many :colors
-	has_many :breeds
+	belongs_to :breed
 	belongs_to :pet_location
 	belongs_to :pet_image
 	belongs_to :species
--- PoundBackend/app/serializers/pet_serializer.rb
+++ PoundBackend/app/serializers/pet_serializer.rb
@@ -1,4 +1,4 @@
 class PetSerializer < ActiveModel::Serializer
   embed :ids, :include => true
-  attributes :id, :name, :image_url, :thumb_url, :species_id, :breed_id, :reward, :notes, :contact_name, :contact_detail, :contact_type, :lost, :colors, :pet_location
+  attributes :id, :name, :image_url, :thumb_url, :species_id, :species, :breed_id, :breed, :reward, :notes, :contact_name, :contact_detail, :contact_type, :lost, :colors, :color_ids, :pet_location
 end
